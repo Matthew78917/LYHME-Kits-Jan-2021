584c11c1ee21b7575e91a2bd0fccdafdc1dbb3dc
Matthew <54089351+Matthew78917@users.noreply.github.com>
2022-01-06 08:49:37 +0800

    Added sql commands

---
+++ kits.sql
@@ -0,0 +1,29 @@
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Needs', '[MEMBER]', '1158 290 378 1098', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Starter', '[MEMBER]', '161 2 276 14/3 13/2 1175', '0', '0', '14400');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Vehicle', '[MEMBER]', '28/3 1450/2 1451/2 277', '0', '0', '14400');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Medic', '[MEMBER]', '394/3 95/5 269/4 15/2 387/4', '0', '0', '10800');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('XP', '[MEMBER]', 'xp.1000', '0', '0', '86400');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Russian', '[MEMBER]', '334 1333 1334 1362 125 1369 1371 1004 19430/2', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Hunter', '[MEMBER]', '245 1018 1020 461/2 468 14/2 209 171', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Specops', '[MEMBER]', '425 334 1377 125 259/2 261/2 333', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Scavenger', '[MEMBER]', '1199 1391 1392 1037 6 146 13/2 80/2 333', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Assassin', '[MEMBER]', '182 214 395 13/4 1021 1022 7 146 151 261/2', '0', '0', '14400');
+
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('VIP', '[VIP]', '253 467/3 14/3 28/2 43 44 76 490', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Assault', '[VIP]', '309 307 308 310 253 1037 17 254/2 13/2 146 8 149', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('SMG', '[VIP]', '1010 1011 1012 1013 253 1379 1381 146', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Sniper', '[VIP]', '253 1021 1022 296 7 1008 143 395/2', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Attachment', '[VIP]', '146 152 148 8 151 1008', '0', '0', '21600');
+
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('VIPPlus', '[VIPPlus]', '253 29 76 104 43/2 44/2 119', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Gas', '[VIPPlus]', '277 28 76 1450', '0', '0', '14400');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Military', '[VIPPlus]', '334 307 308 310 8 43 333 395 121', '0', '0', '21600');
+
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('MVP', '[MVP]', '237 334 235 236 238 253 1018 1020 43/2 490 28/2', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Bandit', '[MVP]', '1156 1157 1369 1371 88/3 14/3', '0', '0', '14400');
+
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('MVPPlus', '[MVPPlus]', '334 1354 1355 1356 1357 253 1018 1020 43/2 44/2 490 28/2', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Sentry', '[MVPPlus]', '1199 407 408 1222/2 76 28 458 1244/2', '0', '0', '21600');
+
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Deluxe', '[DELUXE]', '163 209 310 253 1271/3 209 1008 296 1192 1241/2', '0', '0', '21600');
+INSERT INTO `LYHMEut`.`kits` (`Name`, `Category`, `Content`, `Priority`, `Cost`, `Cooldown`) VALUES ('Heavy', '[DELUXE]', '309 307 308 310 334', '0', '0', '21600');
